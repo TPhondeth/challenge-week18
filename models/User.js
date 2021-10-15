4a129078e81e6039863a2fd03e90d47d95a8fe8b
Tony Phondeth <phondeth@gmail.com>
2021-10-15 15:22:00 -0400

    update User model

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const {
     Schema,
     model
 } = require('mongoose');
+const moment = require('moment');
 
 const UserSchema = new Schema({
     username: {
@@ -16,18 +17,24 @@ const UserSchema = new Schema({
         required: true,
         match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
     },
-    thoughts: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Thought'
-        }
-    ],
-    friends: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        }
-    ]
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+// Virtual property friendCount
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
 });
 
 // Creates User model with Schema
